f4c3ab27a7a1b62c7d2f71b447725785fc6b2f68
Valeriy Avanesov <acopich@gmail.com>
2020-07-25 22:54:39 +0200

    DVSemigroup and DMSemigroup are traits now.

--- src/main/scala/scalapurerandom/BreezeInstances.scala
+++ src/main/scala/scalapurerandom/BreezeInstances.scala
@@ -2,10 +2,10 @@ package scalapurerandom
 
 import algebra.ring.AdditiveSemigroup
 
-class DVSemigroup extends AdditiveSemigroup[DV] {
+trait DVSemigroup extends AdditiveSemigroup[DV] {
   override def plus(x: DV, y: DV): DV = x + y
 }
 
-class DMSemigroup extends AdditiveSemigroup[DM] {
+trait DMSemigroup extends AdditiveSemigroup[DM] {
   override def plus(x: DM, y: DM): DM = x + y
 }
